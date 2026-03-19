52055d3ea0fadccd60a658d0d12439abbbd3d985
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-03-19 11:18:22 +0530

    Add. Model in which user class is being created

---
+++ models/user.py
@@ -0,0 +1,8 @@
+class User:
+
+    def __init__(self,first_name,last_name,email,phone_number,password):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.phone_number=phone_number
+        self.password=password
